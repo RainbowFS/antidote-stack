d396abc2b8440730f2c95b715275d5dd4a0049db
Nicolas Herbaut <nicolas.herbaut@univ-grenoble-alpes.fr>
2018-05-14 17:25:51 +0200

    salt

--- mcc/install-with-mcc.sh
+++ mcc/install-with-mcc.sh
@@ -2,11 +2,14 @@
 #staring 5 machines for 2 hours in nancy starting now
 export JOB_ID=`mcc job add nancy 3 for 2h now`
 
+echo $JOB_ID > .jobid
+
 #wait for the machines to be allocated
 mcc job wait $JOB_ID
 
 #deploy the OS on all the target machines
 export DEP_ID=`mcc dep add $JOB_ID`
+echo $DEP_ID > .depid
 
 #wait for the deployment to complete
 mcc dep wait $DEP_ID
@@ -17,3 +20,4 @@ mcc job install $JOB_ID salt
 #create an alias file for direct ssh access
 mcc alias list $JOB_ID > alias
 
+exit 0
--- mcc/master.tpl
+++ mcc/master.tpl
@@ -4,12 +4,11 @@ file_recv: True
 file_roots:
   base:
     - /srv/salt
+    - /srv/formulas/monitoring-formula
+    - /srv/formulas/docker-formula
+    - /srv/formulas/bind9-formula
 fileserver_backend:
   - minion
   - roots
   - git
 gitfs_provider: gitpython
-gitfs_remotes:
-  - https://gricad-gitlab.univ-grenoble-alpes.fr/herbautn/monitoring-formula.git
-  - https://gricad-gitlab.univ-grenoble-alpes.fr/herbautn/docker-formula.git
-  - https://github.com/nherbaut/bind9-formula.git
