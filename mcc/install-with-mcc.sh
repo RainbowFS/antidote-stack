90271fe1bb8ad229244351520e496ebf45c85285
Nicolas Herbaut <nicolas.herbaut@univ-grenoble-alpes.fr>
2018-05-28 11:52:23 +0200

    more logs

--- mcc/install-with-mcc.sh
+++ mcc/install-with-mcc.sh
@@ -4,6 +4,7 @@ export JOB_ID=`mcc job add nancy 8 for 2h  now`
 
 echo $JOB_ID > .jobid
 
+echo "job creation: "
 #wait for the machines to be allocated
 mcc job wait $JOB_ID
 
@@ -13,10 +14,12 @@ echo "**************"
 export DEP_ID=`mcc dep add $JOB_ID`
 echo $DEP_ID > .depid
 
+echo "deployment creation: "
 #wait for the deployment to complete
 mcc dep wait $DEP_ID
 echo "**************"
 
+echo "nodes installation: "
 #install salt and antidote receipes from settings.yaml
 mcc job install $JOB_ID salt
 
